8535e0e9fd8d71dba6d12fc4dbe661016b7d374c
Kevin Martensson <kevinmartensson@gmail.com>
2014-10-21 16:38:53 +0200

    Provide a reusable constructor

--- index.js
+++ index.js
@@ -2,11 +2,77 @@
 
 var isPng = require('is-png');
 var spawn = require('child_process').spawn;
-var through2 = require('through2');
+var through = require('through2');
 var pngout = require('pngout-bin').path;
 
 /**
  * pngout imagemin plugin
+ *
+ * @param {Object} file
+ * @param {String} enc
+ * @param {Object} opts
+ * @param {Function} cb
+ * @api private
+ */
+
+function plugin(file, enc, opts, cb) {
+	if (file.isNull()) {
+		cb(null, file);
+	}
+
+	if (file.isStream()) {
+		cb(new Error('Streaming is not supported'));
+		return;
+	}
+
+	if (!isPng(file.contents)) {
+		cb(null, file);
+		return;
+	}
+
+	var args = ['-', '-', '-y', '-force'];
+	var err = '';
+	var ret = [];
+	var len = 0;
+
+	if (typeof opts.strategy === 'number') {
+		args.push('-s', opts.strategy);
+	}
+
+	var cp = spawn(pngout, args);
+
+	cp.stderr.on('error', function (err) {
+		cb(err);
+	});
+
+	cp.stderr.setEncoding('utf8');
+	cp.stderr.on('data', function (data) {
+		err += data;
+	});
+
+	cp.stdout.on('data', function (data) {
+		ret.push(data);
+		len += data.length;
+	});
+
+	cp.on('close', function (code) {
+		if (code) {
+			cb(new Error(err));
+			return;
+		}
+
+		if (len < file.contents.length) {
+			file.contents = Buffer.concat(ret, len);
+		}
+
+		cb(null, file);
+	});
+
+	cp.stdin.end(file.contents);
+}
+
+/**
+ * Module exports
  *
  * @param {Object} opts
  * @api public
@@ -15,59 +81,22 @@ var pngout = require('pngout-bin').path;
 module.exports = function (opts) {
 	opts = opts || {};
 
-	return through2.obj(function (file, enc, cb) {
-		if (file.isNull()) {
-			cb(null, file);
-		}
+	return through.obj(function (file, enc, cb) {
+		plugin(file, enc, opts, cb);
+	});
+};
 
-		if (file.isStream()) {
-			cb(new Error('Streaming is not supported'));
-			return;
-		}
+/**
+ * Module exports constructor
+ *
+ * @param {Object} opts
+ * @api public
+ */
 
-		if (!isPng(file.contents)) {
-			cb(null, file);
-			return;
-		}
+module.exports.ctor = function (opts) {
+	opts = opts || {};
 
-		var args = ['-', '-', '-y', '-force'];
-		var err = '';
-		var ret = [];
-		var len = 0;
-
-		if (typeof opts.strategy === 'number') {
-			args.push('-s', opts.strategy);
-		}
-
-		var cp = spawn(pngout, args);
-
-		cp.stderr.on('error', function (err) {
-			cb(err);
-		});
-
-		cp.stderr.setEncoding('utf8');
-		cp.stderr.on('data', function (data) {
-			err += data;
-		});
-
-		cp.stdout.on('data', function (data) {
-			ret.push(data);
-			len += data.length;
-		});
-
-		cp.on('close', function (code) {
-			if (code) {
-				cb(new Error(err));
-				return;
-			}
-
-			if (len < file.contents.length) {
-				file.contents = Buffer.concat(ret, len);
-			}
-
-			cb(null, file);
-		});
-
-		cp.stdin.end(file.contents);
+	return through.ctor({ objectMode: true }, function (file, enc, cb) {
+		plugin(file, enc, opts, cb);
 	});
 };
